98411cdcb1e6eeb25c093fafa3a7122250e7f5e7
InTheMorning <maikiampietro@gmail.com>
2017-02-18 23:57:16 -0500

    cleaned up messiness

--- main.py
+++ main.py
@@ -1,43 +1,37 @@
+import time,pywapi,json,string,threading
 import I2C_LCD_driver as i2c_charLCD
-import time
-import pywapi
-from pprint import pprint
-from rotary_class import RotaryEncoder
 import RPi.GPIO as GPIO
+from rotary_class import RotaryEncoder
 from bme280 import readBME280All 
-import json  
-import string
-import threading
 
-# Define Rotary GPIO inputs
-PIN_A = 11 
-PIN_B = 13
+# Define GPIO inputs
+rotaryA = 11 
+rotaryB = 13
 BUTTON = 15
 
-# Define Relay outputs
-RELAY_A = 7
+# Define GPIO outputs
+relay1 = 7
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(RELAY_A, GPIO.OUT, initial=GPIO.LOW)
-#GPIO.setup(RELAY_B, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(PIN_A, GPIO.IN)
-GPIO.setup(PIN_B, GPIO.IN)
+GPIO.setup(relay1, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(rotaryA, GPIO.IN)
+GPIO.setup(rotaryB, GPIO.IN)
 
-
-result,stemp,spressure,shumidity=0,0,0,0
-forecast_day=0
-
+# Initialiaze variables
+forecast_day,latest_weather,stemp,spressure,shumidity=0,0,0,0,0
 
 mylcd = i2c_charLCD.lcd()
 mylcd.backlight(1)
 
-def getweather():
+
+def getweather():  
+# Get weather from weather API
     while True:
-      global result
-      result = pywapi.get_weather_from_weather_com('CAXX2224:1:CA', units = 'metric' )
+      global latest_weather
+      latest_weather = pywapi.get_weather_from_weather_com('CAXX2224:1:CA', units = 'metric' )
       time.sleep(900)
 
-def getsensordata(samples,refresh):
+def smoothsensordata(samples,refresh):
     global stemp,spressure,shumidity
     stemp,spressure,shumidity = readBME280All()
     while True:
@@ -49,7 +43,8 @@ def getsensordata(samples,refresh):
       stemp,spressure,shumidity=t/samples,p/samples,h/samples
 
 weatherthread = threading.Thread(target=getweather)
-sensorthread = threading.Thread(target=getsensordata,args=(5,30))
+sensorthread = threading.Thread(target=smoothsensordata,args=(5,30))
+
 weatherthread.start()
 sensorthread.start()
 
@@ -57,13 +52,13 @@ sensorthread.start()
 def switch_event(event):
         global forecast_day
         if event == RotaryEncoder.CLOCKWISE:
-          GPIO.output(RELAY_A, GPIO.HIGH)
+          GPIO.output(relay1, GPIO.HIGH)
           if forecast_day < 3:
             forecast_day = forecast_day + 1
           else:
             forecast_day = 4 
         elif event == RotaryEncoder.ANTICLOCKWISE:
-          GPIO.output(RELAY_A, GPIO.LOW)
+          GPIO.output(relay1, GPIO.LOW)
           if forecast_day > 1:
             forecast_day=forecast_day-1
           else:
@@ -75,9 +70,7 @@ def switch_event(event):
         return
 
 # Define the switch
-rswitch = RotaryEncoder(PIN_A,PIN_B,BUTTON,switch_event)
-
-
+rswitch = RotaryEncoder(rotaryA,rotaryB,BUTTON,switch_event)
 
 
 
@@ -85,18 +78,18 @@ try:
   while 1:
     localtime = time.asctime(time.localtime(time.time()))
     
-    while result == 0:
+    while latest_weather == 0:
       print ("waiting for external weather info")
       time.sleep(1)
    
-    outtempraw = int(result['current_conditions'] ['temperature'])
+    outtempraw = int(latest_weather['current_conditions'] ['temperature'])
     outtemp = '{0:.1f}'.format(outtempraw) + chr(223) +"C"
-    cc = result['current_conditions']['text']
-    outhumidity = int(result['current_conditions']['humidity'])
-    dayofweek = result['forecasts'][forecast_day]['day_of_week']
-    date = result['forecasts'][forecast_day]['date']
-    high = result['forecasts'][forecast_day]['high'] + chr(223) + "C" 
-    low = result['forecasts'][forecast_day]['low'] + chr(223) + "C"
+    cc = latest_weather['current_conditions']['text']
+    outhumidity = int(latest_weather['current_conditions']['humidity'])
+    dayofweek = latest_weather['forecasts'][forecast_day]['day_of_week']
+    date = latest_weather['forecasts'][forecast_day]['date']
+    high = latest_weather['forecasts'][forecast_day]['high'] + chr(223) + "C" 
+    low = latest_weather['forecasts'][forecast_day]['low'] + chr(223) + "C"
     sensortemp = '{0:.1f}'.format(stemp) + chr(223) + "C"
 
     print ("refreshing screen")
@@ -114,5 +107,6 @@ except KeyboardInterrupt:
   weatherthread.stop()
   sensorthread.stop()
   GPIO.cleanup()
-  print("exited")
-  raise
+GPIO.cleanup()
+weatherthread.stop()
+sensorthread.stop()
