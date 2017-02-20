c158a3fb83c67efaf5c142a7483d24de3bced30f
InTheMorning <maikiampietro@gmail.com>
2017-02-20 11:54:31 -0500

    Added bouncetime to rotary class

--- rotary_class.py
+++ rotary_class.py
@@ -11,6 +11,7 @@
 #
 
 import RPi.GPIO as GPIO
+import time
 
 class RotaryEncoder:
 
@@ -24,6 +25,9 @@ class RotaryEncoder:
 	rotary_c = 0
 	last_state = 0
 	direction = 0
+
+	bouncetime = 0.125 	# Rotary bouncetime
+	timestamp = time.time()
 
 	# Initialise rotary encoder object
 	def __init__(self,pinA,pinB,button,callback):
@@ -55,36 +59,41 @@ class RotaryEncoder:
 
 	# Call back routine called by switch events
 	def switch_event(self,switch):
-		if GPIO.input(self.pinA):
-			self.rotary_a = 1
-		else:
-			self.rotary_a = 0
+		global bouncetime, timestamp
+		time_now = time.time()
+		if (time_now - timestamp) >= bouncetime:
 
-		if GPIO.input(self.pinB):
-			self.rotary_b = 1
-		else:
-			self.rotary_b = 0
-
-		self.rotary_c = self.rotary_a ^ self.rotary_b
-		new_state = self.rotary_a * 4 + self.rotary_b * 2 + self.rotary_c * 1
-		delta = (new_state - self.last_state) % 4
-		self.last_state = new_state
-		event = 0
-
-		if delta == 1:
-			if self.direction == self.CLOCKWISE:
-				# print "Clockwise"
-				event = self.direction
+			if GPIO.input(self.pinA):
+				self.rotary_a = 1
 			else:
-				self.direction = self.CLOCKWISE
-		elif delta == 3:
-			if self.direction == self.ANTICLOCKWISE:
-				# print "Anticlockwise"
-				event = self.direction
+				self.rotary_a = 0
+
+			if GPIO.input(self.pinB):
+				self.rotary_b = 1
 			else:
-				self.direction = self.ANTICLOCKWISE
-		if event > 0:
-			self.callback(event)
+				self.rotary_b = 0
+
+			self.rotary_c = self.rotary_a ^ self.rotary_b
+			new_state = self.rotary_a * 4 + self.rotary_b * 2 + self.rotary_c * 1
+			delta = (new_state - self.last_state) % 4
+			self.last_state = new_state
+			event = 0
+
+			if delta == 1:
+				if self.direction == self.CLOCKWISE:
+					# print "Clockwise"
+					event = self.direction
+				else:
+					self.direction = self.CLOCKWISE
+			elif delta == 3:
+				if self.direction == self.ANTICLOCKWISE:
+					# print "Anticlockwise"
+					event = self.direction
+				else:
+					self.direction = self.ANTICLOCKWISE
+			if event > 0:
+				self.callback(event)
+		timestamp = time_now
 		return
 
 
