9ee66e91fff71fb1f0d169a9d1ae5dbaec2cd4dc
HeyCitizen <4637290+InTheMorning@users.noreply.github.com>
2020-02-27 20:15:30 -0500

    Formatting fixes

--- rotarytest.py
+++ rotarytest.py
@@ -1,7 +1,8 @@
-import time
-from rotary_class import RotaryEncoder
 import RPi.GPIO as GPIO
-import random
+import time
+
+from rotary_class import RotaryEncoder
+
 
 # Define GPIO inputs
 PIN_A = 11
@@ -11,35 +12,32 @@ BUTTON = 13
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT)
 
-tone=GPIO.PWM(12, 2000)
+tone = GPIO.PWM(12, 2000)
 tone.start(0)
 
 
-# This is the event callback routine to handle events
 def switch_event(event):
-        if event == RotaryEncoder.CLOCKWISE:
-          print ("--->>>")
-        elif event == RotaryEncoder.ANTICLOCKWISE:
-          print ("<<<---")
-        elif event == RotaryEncoder.BUTTONDOWN:
-          print ("  __   ")
-        elif event == RotaryEncoder.BUTTONUP:
-          print ("  [^]  ")
+    # This is the event callback routine to handle events
+    if event == RotaryEncoder.CLOCKWISE:
+        print("--->>>")
+    elif event == RotaryEncoder.ANTICLOCKWISE:
+        print("<<<---")
+    elif event == RotaryEncoder.BUTTONDOWN:
+        print("  __   ")
+    elif event == RotaryEncoder.BUTTONUP:
+        print("  [^]  ")
 
-        tone.ChangeDutyCycle(100)
-        time.sleep(0.02)
-        tone.ChangeDutyCycle(0)
-        return
-
-# Define the switch
-rswitch = RotaryEncoder(PIN_A,PIN_B,BUTTON,switch_event)
+    tone.ChangeDutyCycle(100)
+    time.sleep(0.02)
+    tone.ChangeDutyCycle(0)
+    return
 
 
-
+rswitch = RotaryEncoder(PIN_A, PIN_B, BUTTON, switch_event)
 
 try:
-  while 1:
-    time.sleep(0.1)
+    while True:
+        time.sleep(0.1)
 except KeyboardInterrupt:
     pass
 tone.stop()
